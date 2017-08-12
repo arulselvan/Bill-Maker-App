c28eb9f3b4b0ca043f6f73b3a0f43fef8d25e07f
sudhakar <sssudhakar1985@gmail.com>
2017-08-12 22:02:53 +0530

    text search UI implemetation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2CompleterModule } from 'ng2-completer';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { BillingComponent } from './billing/billing';
+import { SearchComponent } from './billing/billing.search';
 
 @NgModule({
-  declarations: [    AppComponent  ],
+  declarations: [    AppComponent,
+  BillingComponent,
+    SearchComponent  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2CompleterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
